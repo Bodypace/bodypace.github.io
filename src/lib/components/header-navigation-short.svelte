6626ccafbc67819f7d2fa2ee608f7e3a3197d206
Robert Dorna <rdorna8@gmail.com>
2023-08-18 22:09:53 +0200

    feat: add drop down navigation for smaller screens

---
+++ src/lib/components/header-navigation-short.svelte
@@ -0,0 +1,102 @@
+<script>
+    export let selected;
+</script>
+
+<nav>
+    <ul>
+        <!-- <li>
+            <a href="/" class:selected={selected === "home"}>Home</a>
+        </li>
+        <li>
+            <a href="/downloads" class:selected={selected === "download"}>Download apps</a>
+        </li>
+        <li>
+            <a href="/future" class:selected={selected === "future"}>Future plans</a>
+        </li>
+        <li>
+            <a href="/development" class:selected={selected === "development"}>Development</a>
+        </li> -->
+    </ul>
+    <div class="spacer" />
+    <ul>
+        <i class="fas fa-bars" on:click on:keypress={undefined} role="button" tabindex="0" />
+        <!-- <li>
+            <a href="">Language</a>
+        </li> -->
+        <!-- <li>
+            <a href="/todo_webapp">Login</a>
+        </li> -->
+        <!-- <li>
+            <a href="/todo_webapp" class="highlight" class:selected={selected === "webapp"}>Online account</a>
+        </li> -->
+    </ul>
+</nav>
+
+<style>
+    i {
+        /* width: 64px;
+        height: 64px; */
+        /* background-color: brown; */
+        font-size: 24px;
+        align-self: center;
+        color: #333;
+    }
+
+    nav {
+        display: flex;
+        flex: 1;
+    }
+
+    nav ul {
+        padding: 0;
+        margin: 0 20px;
+        display: flex;
+        /* background-color: brown; */
+        list-style-type: none;
+    }
+
+    nav ul li a {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        /* flex: 1; */
+        height: 100%;
+        padding: 0 20px;
+        text-decoration: none;
+        color: inherit;
+    }
+
+    nav ul li a:hover {
+        /* background-color: black; */
+        /* background-color: #f55557; */
+        background-color: #f0f0f0;
+        /* box-shadow: 8px 8px 24px 0px rgba(244, 240, 240, 1); */
+    }
+
+    nav ul li a:hover.selected {
+        background-color: #ebebeb;
+    }
+
+    .spacer {
+        display: flex;
+        flex: 1;
+        /* background-color: rosybrown; */
+        /* height: 10px; */
+        /* width: 200px; */
+    }
+
+    .selected {
+        /* text-decoration: underline; */
+        /* border-left: 1px solid silver; */
+        /* border-right: 1px solid silver; */
+        background-color: #ebebeb;
+    }
+
+    .highlight {
+        /* background-color: #f55557; */
+        /* color: white; */
+        /* color: white; */
+        /* color: #f55557; */
+        /* font-weight: bold; */
+    }
+</style>
--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,22 +1,120 @@
 <script>
     import Logo from "./header-logo.svelte";
-    import Navigation from "./header-navigation.svelte";
+    import NavigationLong from "./header-navigation-long.svelte";
+    import NavigationShort from "./header-navigation-short.svelte";
 
     export let selected;
+
+    let menu = false;
+    function toggleMenu() {
+        menu = !menu;
+    }
 </script>
 
-<header>
+<header class="long">
     <Logo />
-    <Navigation {selected} />
+    <NavigationLong {selected} />
+</header>
+<header class="short">
+    <Logo />
+    <NavigationShort {selected} on:click={toggleMenu} />
 </header>
 
+<!-- <div class="test"> -->
+<div class:hidden={!menu} class:menu>
+    <ul>
+        <li>
+            <a href="/" class:selected={selected === "home"}>Home</a>
+        </li>
+        <li>
+            <a href="/downloads" class:selected={selected === "download"}>Download apps</a>
+        </li>
+        <li>
+            <a href="/future" class:selected={selected === "future"}>Future plans</a>
+        </li>
+        <li>
+            <a href="/development" class:selected={selected === "development"}>Development</a>
+        </li>
+        <li>
+            <a href="/todo_webapp" class="highlight" class:selected={selected === "webapp"}>Online account</a>
+        </li>
+        <li>
+            <a href="/regulations" class:selected={selected === "regulations"}>Terms and conditions</a>
+        </li>
+        <li>
+            <a href="/policy" class:selected={selected === "policy"}>Privacy Policy</a>
+        </li>
+    </ul>
+</div>
+
+<!-- </div> -->
+
 <style>
+    /* header common */
     header {
         position: sticky;
         top: 0;
         display: flex;
         border-bottom: 1px solid silver;
         background-color: white;
-        overflow: scroll;
+    }
+
+    .short {
+        display: none;
+    }
+
+    @media screen and (max-width: 1300px) {
+        .long {
+            display: none;
+        }
+
+        .short {
+            display: flex;
+        }
+    }
+
+    /* mobile navigation */
+    .hidden {
+        display: none;
+    }
+    .menu {
+        position: sticky;
+        top: 63px;
+        right: 0;
+        z-index: 5;
+        display: flex;
+        flex-direction: column;
+        background-color: white;
+        border-bottom: 1px solid silver;
+    }
+
+    ul {
+        margin: 0;
+        padding: 20px 0;
+        list-style-type: none;
+    }
+
+    li {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    a {
+        padding: 5px 10px;
+        margin: 10px 5px;
+        color: inherit;
+        text-decoration: none;
+        font-size: 20px;
+
+        border-left: 1px solid silver;
+        width: 80vw;
+        background-color: #fafafa;
+    }
+
+    .selected {
+        border-left: 4px solid black;
+        font-weight: bold;
+        background-color: #f0f0f0;
     }
 </style>
